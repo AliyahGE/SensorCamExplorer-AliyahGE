4384bd933fe5d6f0ed0dfd9558ffaed15775aca9
AliyahGE <aliyahgutierrezexposito@gmail.com>
2026-05-26 12:53:17 +0200

    Afegir app

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-5ccb27be'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ct30h7as85g"
+    "revision": "0.316681ldo6o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
